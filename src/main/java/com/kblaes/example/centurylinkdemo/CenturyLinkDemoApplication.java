c55c1666e0363ab42c4b71e4369c7c8161f76ead
Kyle Blaes <kblaes@gmail.com>
2019-06-26 08:50:16 -0500

    Initial commit

---
+++ src/main/java/com/kblaes/example/centurylinkdemo/CenturyLinkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kblaes.example.centurylinkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenturyLinkDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CenturyLinkDemoApplication.class, args);
+    }
+
+}
